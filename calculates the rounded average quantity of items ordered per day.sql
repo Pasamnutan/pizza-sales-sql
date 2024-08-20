dbaff85aaf3a55328188eefc8643e7d4de248d6d
Pasamnutan <115466062+Pasamnutan@users.noreply.github.com>
2024-08-20 21:11:50 +0530

    Delete calculates the rounded average quantity of items ordered per day.sql

--- calculates the rounded average quantity of items ordered per day.sql
+++
@@ -1,9 +0,0 @@
--- This query calculates the rounded average quantity of items ordered per day.
-
-SELECT ROUND(AVG(total_quantity), 0) AS average_quantity
-FROM (
-  SELECT SUM(order_details.quantity) AS total_quantity
-  FROM orders
-  JOIN order_details ON orders.order_id = order_details.order_id
-  GROUP BY orders.order_date
-) AS order_quantity;
