5a50011df54f7327e03934a4b5a5b9c4817bbb96
Pasamnutan <115466062+Pasamnutan@users.noreply.github.com>
2024-08-21 15:09:50 +0530

    Delete PIZZA SALES SQL directory

--- PIZZA SALES SQL/revenue generated from pizza sales.sql
+++
@@ -1,2 +0,0 @@
-#Calculate the total revenue generated from pizza sales
-select round(sum(order_details.quantity * pizzas.price),2)  as total_sales from order_details join pizzas  on pizzas.pizza_id = order_details.pizza_id ;
